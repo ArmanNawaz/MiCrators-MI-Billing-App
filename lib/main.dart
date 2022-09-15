d85e80947de58198de28bc8aedb1df3b61488bd5
ArmanNawaz <glintarman@gmail.com>
2022-09-15 22:09:44 +0530

    Windows support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mi_crators/screens/home_page.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Mi-Crators',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,14 +1,25 @@
+import 'dart:async';
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:get/get.dart';
 import 'package:mi_crators/constants.dart';
 import 'package:mi_crators/screens/dashboard.dart';
 
-List? inputs = List.filled(3, "");
-// inputs[0] = username, input[1] = password, input[2] = posId
+TextEditingController username = TextEditingController();
+TextEditingController password = TextEditingController();
+TextEditingController posId = TextEditingController();
+
+class HomePageController extends GetxController {
+  final isLoading = false.obs;
+  switchLoading() => isLoading.value = !isLoading.value;
+}
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+  final HomePageController controller = Get.put(HomePageController());
 
   @override
   Widget build(BuildContext context) {
@@ -16,98 +27,124 @@ class MyHomePage extends StatelessWidget {
     double pd = size.width >= 700 ? 0.4296875 * size.width : 0.34 * size.width;
     double textInputSize = size.width * 0.390625;
     return Scaffold(
-      body: Container(
-        color: primaryColor,
-        child: ListView(
-          children: [
-            const SizedBox(height: 50),
-            SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          Container(
+            color: primaryColor,
+            child: ListView(
+              children: [
+                const SizedBox(height: 50),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Container(
-                        height: max(size.width * 0.1, 100) + 25,
-                        width: max(size.width * 0.1, 100) + 25,
-                        decoration: BoxDecoration(
-                          color: primaryColor,
-                          image: const DecorationImage(
-                            image: AssetImage('images/app_log.png'),
-                            fit: BoxFit.fitWidth,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: max(size.width * 0.1, 100) + 25,
+                            width: max(size.width * 0.1, 100) + 25,
+                            decoration: BoxDecoration(
+                              color: primaryColor,
+                              image: const DecorationImage(
+                                image: AssetImage('images/app_log.png'),
+                                fit: BoxFit.fitWidth,
+                              ),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Color(0xffe35d00),
+                                  blurRadius: 20,
+                                  spreadRadius: 0.0,
+                                  offset: Offset(20, 20),
+                                ),
+                                BoxShadow(
+                                  color: Color(0xffff7500),
+                                  blurRadius: 20,
+                                  spreadRadius: 0.0,
+                                  offset: Offset(-20, -20),
+                                ),
+                              ],
+                              borderRadius: BorderRadius.circular(
+                                max(size.aspectRatio * 25, 10),
+                              ),
+                            ),
                           ),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Color(0xffe35d00),
-                              blurRadius: 20,
-                              spreadRadius: 0.0,
-                              offset: Offset(20, 20),
+                        ],
+                      ),
+                      const SizedBox(height: 40),
+                      createInputFields(
+                          isPc: size.width > 900, textInputSize: textInputSize),
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding:
+                            EdgeInsets.only(top: 10.0, left: pd, right: pd),
+                        child: SizedBox(
+                          height: 40,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              bool valid = isValid();
+                              controller.switchLoading();
+                              Timer(const Duration(seconds: 3), () {
+                                controller.switchLoading();
+                                if (valid) {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const DashBoard(),
+                                    ),
+                                  );
+                                } else {
+                                  Get.snackbar(
+                                    "Error",
+                                    "Invalid details",
+                                    snackPosition: SnackPosition.BOTTOM,
+                                    backgroundColor: Colors.red,
+                                    margin: const EdgeInsets.all(10),
+                                    icon:
+                                        const Icon(Icons.warning_amber_rounded),
+                                  );
+                                }
+                              });
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: primaryColor,
+                              foregroundColor: Colors.black,
+                              elevation: 20.0,
+                              shadowColor: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              side: const BorderSide(
+                                color: Colors.black,
+                                width: 0.6,
+                              ),
                             ),
-                            BoxShadow(
-                              color: Color(0xffff7500),
-                              blurRadius: 20,
-                              spreadRadius: 0.0,
-                              offset: Offset(-20, -20),
+                            child: const SizedBox(
+                              width: 150.0,
+                              child: Center(
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                              ),
                             ),
-                          ],
-                          borderRadius: BorderRadius.circular(
-                            max(size.aspectRatio * 25, 10),
                           ),
                         ),
                       ),
+                      const SizedBox(height: 1.0)
                     ],
                   ),
-                  const SizedBox(height: 40),
-                  createInputFields(
-                      isPc: size.width > 900, textInputSize: textInputSize),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: EdgeInsets.only(top: 10.0, left: pd, right: pd),
-                    child: SizedBox(
-                      height: 40,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Future.delayed(const Duration(seconds: 10));
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const DashBoard()));
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: primaryColor,
-                          foregroundColor: Colors.black,
-                          elevation: 20.0,
-                          shadowColor: Colors.black,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          side: const BorderSide(
-                            color: Colors.black,
-                            width: 0.6,
-                          ),
-                        ),
-                        child: const SizedBox(
-                          width: 150.0,
-                          child: Center(
-                            child: Text(
-                              "Login",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 20),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 1.0)
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Obx(() => LoadingOverlay(
+                isLoading: controller.isLoading.value,
+              ))
+        ],
       ),
     );
   }
@@ -121,52 +158,64 @@ Widget createInputFields({bool? isPc, double? textInputSize}) {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             _textInput(
-                text: "Username",
-                width: textInputSize,
-                isPass: false,
-                inputValue: 0),
+              text: "Username",
+              width: textInputSize,
+              isPass: false,
+              controller: username,
+            ),
             _textInput(
-                text: "Password",
-                width: textInputSize,
-                isPass: true,
-                inputValue: 1)
+              text: "Password",
+              width: textInputSize,
+              isPass: true,
+              controller: password,
+            )
           ],
         ),
         const SizedBox(height: 20),
         _textInput(
-            text: "POS ID", width: textInputSize, isPass: false, inputValue: 2)
+            text: "POS ID",
+            width: textInputSize,
+            isPass: false,
+            controller: posId)
       ],
     );
   } else {
     return Column(
       children: [
         _textInput(
-            text: "Username",
-            width: textInputSize,
-            isPass: false,
-            inputValue: 0),
+          text: "Username",
+          width: textInputSize,
+          isPass: false,
+          controller: username,
+        ),
         _textInput(
-            text: "Password",
-            width: textInputSize,
-            isPass: true,
-            inputValue: 1),
+          text: "Password",
+          width: textInputSize,
+          isPass: true,
+          controller: password,
+        ),
         _textInput(
-            text: "POS ID", width: textInputSize, isPass: false, inputValue: 2)
+          text: "POS ID",
+          width: textInputSize,
+          isPass: false,
+          controller: posId,
+        ),
       ],
     );
   }
 }
 
 Widget _textInput(
-    {String? text, double? width, bool? isPass, int? inputValue}) {
+    {String? text,
+    double? width,
+    bool? isPass,
+    TextEditingController? controller}) {
   return Padding(
     padding: const EdgeInsets.only(top: 30.0),
     child: SizedBox(
       width: width,
       child: TextField(
-        onChanged: (value) {
-          inputs![inputValue!] = value;
-        },
+        controller: controller,
         style: const TextStyle(
             fontSize: 20, fontWeight: FontWeight.bold, letterSpacing: 0.6),
         keyboardType: TextInputType.text,
@@ -185,4 +234,47 @@ Widget _textInput(
       ),
     ),
   );
+}
+
+class LoadingOverlay extends StatelessWidget {
+  const LoadingOverlay({Key? key, this.isLoading}) : super(key: key);
+  final isLoading;
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  SpinKitCircle(
+                    color: Colors.blueGrey,
+                    size: 100,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Authenticating....",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.6),
+                  ),
+                ],
+              ),
+            ),
+          )
+        : const SizedBox(height: 0, width: 0);
+  }
+}
+
+bool isValid() {
+  if (username.text.isEmpty || password.text.isEmpty || posId.text.isEmpty) {
+    return false;
+  }
+  if (username.text == "operator1" &&
+      password.text == "password" &&
+      posId.text == "DLH_01_01_3SDK34") return true;
+  return false;
 }
